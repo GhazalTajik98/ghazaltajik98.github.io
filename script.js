7059bd3a6e148686cd6c6afd347a7eb3946660af
GhazalTajik1998 <ghazal.tajik.77@gmail.com>
2024-03-18 18:31:51 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('download-cv').addEventListener('click', function(event) {
+    // Code to trigger CV download
+    event.preventDefault(); // Prevent default link behavior
+    alert('CV download functionality will be implemented soon.');
+});
